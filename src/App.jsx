afe39e8533fbc9308d971b05c2e005461221647e
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-05-30 17:46:32 -0700

    added Album and Artist

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,58 @@
-import './App.css';
-import { useState, useRef } from 'react'
-import SearchBar from './Components/SearchBar'
+import { Fragment, useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './Components/Gallery'
-import { SearchContext } from './Context/SearchContext'
-import { DataContext } from './Context/DataContext'
+import SearchBar from './Components/SearchBar'
+import AlbumView from './Components/AlbumView'
+import ArtistView from './Components/ArtistView'
+
 
 function App() {
-  let [data, setData] = useState([])
-  let [message, setMessage] = useState("Search for music!")
-  let searchInput = useRef('')
+    let [search, setSearch] = useState('')
+    let [message, setMessage] = useState('Search for Music!')
+    let [data, setData] = useState([])
 
-  const API_URL = 'https://itunes.apple.com/search?term='
+    const API_URL = 'https://itunes.apple.com/search?term='
 
-
-  const handleSearch = (e, term) => {
-    e.preventDefault()
-    document.title = `${term} Music`
-    fetch(API_URL + term)
-      .then(res => res.json())
-      .then(resData => {
-        if (resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage("No results found!")
+    useEffect(() => {
+        if(search) {
+            const fetchData = async () => {
+                document.title = `${search} Music`
+                const response = await fetch(API_URL + search)
+                const resData = await response.json()
+                if (resData.results.length > 0) {
+                    return setData(resData.results)
+                } else {
+                    return setMessage('Not Found')
+                }
+            }
+            fetchData()
         }
-      })
-  }
+    }, [search])
+    
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        setSearch(term)
+    }
 
-  return (
-    <div className="App">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-      <SearchBar/>
-      </SearchContext.Provider>
+    return (
+      <div>
       {message}
-      
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
-    </div>
-  );
+          <Router>
+              <Routes>
+                  <Route path="/" element={
+                      <Fragment>
+                          <SearchBar handleSearch = {handleSearch}/>
+                          <Gallery data={data} />
+                      </Fragment>
+                  } />
+                  <Route path="/album/:id" element={<AlbumView />} />
+                  <Route path="/artist/:id" element={<ArtistView />} />
+              </Routes>
+          </Router>
+      </div>
+  )
+  
 }
 
 export default App;
+
--- src/Components/GalleryItem.jsx
+++ src/Components/GalleryItem.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { link } from 'react-router-dom'
 
 function GalleryItem (props) {
 let [showDetails, setShowDetails] = useState (false)
@@ -33,13 +34,23 @@ const simpleView = () => {
 const detailView = () => {
     return (
         <div style={detailStyle}>
-            <h3>{props.song.trackName}</h3>
-            <h4>{props.song.collectionName}</h4>
-            <h4>{props.song.primaryGenreName}</h4>
-            <h4>{props.song.releaseDate}</h4>
+            <h2>{props.item.trackName}</h2>
+            <h3>
+                <Link to={`/artist/${props.item.artistId}`}>
+                    {props.item.artistName}
+                </Link>
+            </h3>
+            <h3>
+                <Link to={`/album/${props.item.collectionId}`}>
+                    {props.item.collectionName}
+                </Link>
+            </h3>
+            <h4>{props.item.primaryGenreName}</h4>
+            <h4>{props.item.releaseDate}</h4>
         </div>
     )
 }
+
 
     return (
        <div onClick={() => setShowDetails(!showDetails)}
